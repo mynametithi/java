cc1b8f90f10ee4adeff788fe4473a867e7dcf7c3
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-09-16 23:02:32 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,14 @@
+public class unary {
+    public static void main(String[] args) {
+        // int x = 5;
+        // int y = -x;
+        // int z = -y;
+        // System.out.println(z);
+        int a = 5;
+        System.out.println(a++);
+        System.out.println(++a);
+        int b = 6;
+        System.out.println(--b);
+        System.out.println(b--);
+    }
+}
