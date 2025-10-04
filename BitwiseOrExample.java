8ff3e5a841d4372f5ab91f2f354a49f76605b6e7
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-04 22:14:54 +0530

    Add files via upload

---
+++ BitwiseOrExample.java
@@ -0,0 +1,13 @@
+// Program to perform bitwise OR between two numbers
+public class BitwiseOrExample {
+    public static void main(String[] args) {
+        int num1 = 12; // 1100 in binary
+        int num2 = 10; // 1010 in binary
+
+        int result = num1 | num2; // Bitwise OR
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Bitwise OR Result: " + result);
+    }
+}
