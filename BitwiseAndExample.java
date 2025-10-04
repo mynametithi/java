ccfc7b8a2049c55d23896fa768200d4aafc14cea
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-04 22:14:38 +0530

    Add files via upload

---
+++ BitwiseAndExample.java
@@ -0,0 +1,15 @@
+//create a program that shows bitwise and between two numbers:
+
+// Program to perform bitwise AND between two numbers
+public class BitwiseAndExample {
+    public static void main(String[] args) {
+        int num1 = 12; // 1100 in binary
+        int num2 = 10; // 1010 in binary
+
+        int result = num1 & num2; // Bitwise AND
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Bitwise AND Result: " + result);
+    }
+}
