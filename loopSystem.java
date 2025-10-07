330e3bbece9c604ea339760c35bb969dee0fe37e
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-07 20:34:57 +0530

    Add files via upload

---
+++ loopSystem.java
@@ -0,0 +1,9 @@
+public class loopSystem {
+    public static void main(String[] args) {
+        int count = 500;
+        while (count >= 200) {
+            System.out.println(count);
+            count = count - 1;
+        }
+    }
+}
