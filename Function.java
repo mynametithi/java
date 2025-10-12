60beb5af1a25b58af2063e503d1702eb7f5162f9
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-12 22:21:38 +0530

    Add files via upload

--- Function.java
+++ Function.java
@@ -6,5 +6,7 @@ public class Function {
     public static void greedUser() {
         System.out.println("Good morning brother");
         System.out.println("Hello World");
+        System.out.println("Thank you");
+        System.out.println("function for more thing at all");
     }
 }
