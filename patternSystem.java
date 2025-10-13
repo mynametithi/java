781653d979fac91ecef2a4e92b0fd984dfd48aff
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-13 23:18:26 +0530

    Add files via upload

---
+++ patternSystem.java
@@ -0,0 +1,15 @@
+public class patternSystem {
+    public static void main(String[] args) {
+        int[] myArr = new int[5];
+        myArr[0] = 78;
+        myArr[1] = 50;
+        myArr[2] = 67;
+        myArr[3] = 85;
+        myArr[4] = 89;
+        System.out.println(myArr[0]);
+        System.out.println(myArr[1]);
+        System.out.println(myArr[2]);
+        System.out.println(myArr[3]);
+        System.out.println(myArr[4]);
+    }
+}
