9c38f67928cdbfc5a91c0a5b18a3b2e6b3869e25
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-16 22:40:09 +0530

    Add files via upload

---
+++ arrayIndex.java
@@ -0,0 +1,15 @@
+public class arrayIndex {
+    public static void main(String[] args) {
+        int myArr = new int[5];
+        myArr[0] = 98;
+        myArr[1] = 83;
+        myArr[2] = 91;
+        myArr[3] = 65;
+        myArr[4] = 56;
+        System.out.println(myArr[0]);
+        System.out.println(myArr[1]);
+        System.out.println(myArr[2]);
+        System.out.println(myArr[3]);
+        System.out.println(myArr[4]);
+    }
+}
