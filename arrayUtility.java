3b7b875e2fa9fa95016f5168cfe68472fc89adcf
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-19 22:57:38 +0530

    Add files via upload

--- arrayUtility.java
+++ arrayUtility.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class arrayUtility {
     public static void main(String[] args) {
-        
+         Scanner input=new Scanner(System.in);
+          System.out.println("Please enterv the number of elements");
+        int size=input.nextInt();
+        int[] nums=new int[size];
+        int i=0;
+        while(i<size){
+            System.out.println("Please enter the element no"+()+" :");
+            nums[i]=input.nextInt();
+            i++;
+        }
     }
 }
