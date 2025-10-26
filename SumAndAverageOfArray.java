fb27f04c8b41e6ff336193f76fac798d63d0236f
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-26 22:40:49 +0530

    Add files via upload

---
+++ SumAndAverageOfArray.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class SumAndAverageOfArray {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Array sum and average");
+        int[] numArray = ArrayUtility.inputArray();
+    }
+}
