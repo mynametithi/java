a710d0ebcf5df78f3fc1bef535889a2d115c27e4
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-10-28 22:21:02 +0530

    Add files via upload

--- ArrayUtility.java
+++ ArrayUtility.java
@@ -1,17 +1,19 @@
+package challenge;
+
 import java.util.Scanner;
+
 public class ArrayUtility {
     public static int[] inputArray() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the number of elements:");
-
+        System.out.println("Please enter the size of elements:");
         int size = input.nextInt();
-        int[] num = new int[size];
+        int[] nums = new int[size];
         int i = 0;
         while (i < size) {
-            System.out.println("Please enter element number:" + (i + 1) + ":");
-            num[i] = input.nextInt();
+            System.out.println("Please enter element number" + (i + 1) + ":");
+            nums[i] = input.nextInt();
             i++;
         }
-        return num;
+        return nums;
     }
 }
