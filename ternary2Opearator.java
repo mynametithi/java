903ec57ed43d52aa257ec274b06a2902ee61518b
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-01-01 22:37:53 +0530

    Add files via upload

---
+++ ternary2Opearator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ternary2Opearator {
+    
+public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.println("Welcome to number cheaker:");
+    System.out.println("Please enter your first number:");
+    int num1=input.nextInt();
+    System.out.println("Now, enter your second number:");
+    int num2=input.nextInt();
+    int greaterNumber;
+if (num1>num2) {
+    greaterNumber=num1;
+    System.out.println(num1+ " is the greatest number.");
+}
+else{
+    greaterNumber=num2;
+    System.out.println(num2+ " is the greatest number");
+}
+}
+}
