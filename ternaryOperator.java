bbbaa4799c19d072b04d93377f7f4c8348dc699d
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-01-02 23:43:52 +0530

    Add files via upload

--- ternaryOperator.java
+++ ternaryOperator.java
@@ -8,10 +8,13 @@ public static void main(String[] args) {
     int num1=input.nextInt();
     System.out.println("Now, enter your second number:");
     int num2=input.nextInt();
+    int greaterNumber;
 if (num1>num2) {
+    greaterNumber=num1;
     System.out.println(num1+ " is the greatest number.");
 }
 else{
+    greaterNumber=num2;
     System.out.println(num2+ " is the greatest number");
 }
 }
