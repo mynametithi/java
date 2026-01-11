c5ecb66d4c3d6dfc315573938679a0847bc45857
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-01-11 19:37:31 +0530

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,8 @@
+public class forLoop {
+    public static void main(String[] args) {
+        System.out.println("Using for loop print value of simple element:");
+        for(int i=1;i<=100;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
